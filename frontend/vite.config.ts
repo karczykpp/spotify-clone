1a42b8e54679eae559226c0cc080c5030511df23
jakubkarcz <kubkar2004a@gmail.com>
2025-08-12 19:33:55 +0200

    Initial commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,11 @@
+import { fileURLToPath } from "url";
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
